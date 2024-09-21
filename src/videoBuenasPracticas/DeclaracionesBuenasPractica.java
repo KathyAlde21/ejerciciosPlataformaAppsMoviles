b0a5c4d2dbca9de7c61313dff29ba24a6ccb45ef
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-09-20 23:51:02 -0300

    videos

---
+++ src/videoBuenasPracticas/DeclaracionesBuenasPractica.java
@@ -0,0 +1,27 @@
+package videoBuenasPracticas;
+
+public class DeclaracionesBuenasPractica {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        /*
+        int numero1, numero2, numero3; //mal, se deben declarar por separado
+        int numero1;
+        int numero2;
+        int numero3;
+        */
+        
+        //deben ser declarativas, indicar que van a guardar
+        int numeroEntero;
+        
+        //para ver como van los espacios
+        if (true) {
+            
+        } else {
+        }
+        
+        //la indentación y el comentario forman parte de las buenas practicas
+        //el paquete parte con minuscula y el Java con mayúscula      
+        
+    }
+}
---
+++ src/videoScanner/VariablesScanner.java
@@ -0,0 +1,25 @@
+package videoScanner;
+
+//traigo la libreria VariablesScanner
+import java.util.Scanner;
+
+public class VariablesScanner {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        //vamos a trabajar con variables
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("ingrese su nombre");
+        
+        //para que coloque los datos para generar un string
+        
+        String nombre = leer.nextLine();
+        
+        System.out.println("su nombre es: " + nombre);
+        
+        
+        
+        
+    }
+}
